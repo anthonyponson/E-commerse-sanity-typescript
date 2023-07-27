4e3de249fef7cf3546dc1bc286f8afe3c9c1bc8f
Anthony Ponson <antonyponson@gmail.com>
2023-07-27 19:57:12 +0530

    sanity connected

--- components/providers.tsx
+++ components/providers.tsx
@@ -13,12 +13,17 @@ interface Props {
 export function Providers({ children }: Props) {
   return (
     <>
-      <CartProvider 
-      currency="USD"
-      shouldPersist
-      cartMode="checkout-session"
-      
-      >{children}</CartProvider>
+      <CartProvider
+        currency="USD"
+        shouldPersist
+        cartMode="checkout-session"
+        stripe={process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY!}
+      >
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <Toaster />
+          {children}
+        </ThemeProvider>
+      </CartProvider>
     </>
   )
 }
